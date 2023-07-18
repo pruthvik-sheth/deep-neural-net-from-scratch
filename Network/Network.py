7fab7b4c8283f841617b6987b5ab031656277d69
Pruthvik <pns00911@gmail.com>
2023-07-18 17:31:27 +0530

    temp change

--- Network/Network.py
+++ Network/Network.py
@@ -17,6 +17,7 @@ class Network:
         self.loss = loss
         self.loss_prime = loss_prime
 
+    #Predicting
     def predict(self, input_data):
         m = input_data.shape[1]
         y_prediction = np.zeros((1, m))
