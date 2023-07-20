206d7ed8a77fe34933b8fcfda932ce91410d5a58
Pruthvik <pns00911@gmail.com>
2023-07-20 10:22:54 +0530

    formatting changes

--- main.py
+++ main.py
@@ -10,29 +10,34 @@ LEARNING_RATE = 0.002
 REGULARIZATION = True
 LAMBD = 0.95
 
-net = Network()
-input_shape = train_x.shape[0]
-net.add(DeepLayer(input_shape, 64))
-net.add(ActivationLayer(RelU, RelU_prime))
+def main():
+    net = Network()
+    input_shape = train_x.shape[0]
+    net.add(DeepLayer(input_shape, 64))
+    net.add(ActivationLayer(RelU, RelU_prime))
 
-net.add(DeepLayer(64, 64))
-net.add(ActivationLayer(RelU, RelU_prime, dropout=True, keep_prob=0.8))
+    net.add(DeepLayer(64, 64))
+    net.add(ActivationLayer(RelU, RelU_prime, dropout=True, keep_prob=0.8))
 
-net.add(DeepLayer(64, 128))
-net.add(ActivationLayer(RelU, RelU_prime, dropout=True, keep_prob=0.8))
+    net.add(DeepLayer(64, 128))
+    net.add(ActivationLayer(RelU, RelU_prime, dropout=True, keep_prob=0.8))
 
-net.add(DeepLayer(128, 1))
-net.add(ActivationLayer(sigmoid, sigmoid_prime))
+    net.add(DeepLayer(128, 1))
+    net.add(ActivationLayer(sigmoid, sigmoid_prime))
 
-net.use(log_loss, log_loss_prime)
+    net.use(log_loss, log_loss_prime)
 
-net.fit(
-    train_x, 
-    train_y, 
-    test_x, 
-    test_y, 
-    epochs=EPOCHS, 
-    learning_rate=LEARNING_RATE, 
-    regularization=REGULARIZATION, 
-    lambd=LAMBD
-)
+    net.fit(
+        train_x, 
+        train_y, 
+        test_x, 
+        test_y, 
+        epochs=EPOCHS, 
+        learning_rate=LEARNING_RATE, 
+        regularization=REGULARIZATION, 
+        lambd=LAMBD
+    )
+
+if __name__ == "__main__":
+    main()
+    
